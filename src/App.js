e134d5f50dfc29151ec815f8033cde80863b48d7
Kate Endersby <kateende@iastate.edu>
2024-04-03 10:02:16 -0500

    added views

--- src/App.js
+++ src/App.js
@@ -39,59 +39,80 @@ const render_products = (ProductsCategory) => {
   </div>
 }; // end render_products
 
+
+
 const App = () => {
-  console.log("Step 1: Load Products in a useState.");
 
-  const [query, setQuery] = useState('');
-  const [ProductsCategory, setProductsCategory] = useState(Products);
+  const [viewer, setViewer] = useState(0);
 
-  const handleChange = (e) => {
-    setQuery(e.target.value);
-    console.log("Step 6 : in handleChange, Target Value :", e.target.value, " Query Value :", query);
-    const results = Products.filter(eachProduct => {
-      if (e.target.value === "") return ProductsCategory;
-      return eachProduct.title.toLowerCase().includes(e.target.value.toLowerCase())
-    });
-    setProductsCategory(results);
-  }
-
-  function handleClick(tag) {
-    console.log("Step 4 : in handleClick", tag);
-    let filtered = Products.filter(cat => cat.category === tag);
-    // modify useState
-    setProductsCategory(filtered);
-    // ProductsCategory = filtered;
-    console.log("Step 5 : ", Products.length, ProductsCategory.length);
-  }
-
-  console.log("Step 2: Return App :", Products.length, ProductsCategory.length);
-
-  return (
-    <div className="flex fixed flex-row">
-      <div className="h-screen bg-slate-800 p-3 xl:basis-1/5" style={{ minWidth: '65%' }}>
-        {/* <img className="w-full" src={logo} alt="Sunset in the mountains" /> */}
-        <div className="px-6 py-4">
-          <h1 className="text-3xl mb-2 font-bold text-white">Assignment 2 Store </h1>
-          <p className="text-gray-700 text-white">
-            by - <b style={{ color: 'mediumseagreen' }}>Jacob Garcia and Kate Endersby</b>
-          </p>
-          <div className="py-10">
-            <input className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg
-              focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700
-              dark:border-gray-600 dark:placeholder-gray-400 dark:text-white
-              dark:focus:ring-blue-500 dark:focus:border-blue-500" type="search" value={query} onChange={handleChange} />
-              <br />
-            {Categories && <p className='text-white'>Tags : </p>}
-            {Categories.map(tag => <button key={tag} className="inline-block bg-amber-600 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mt-2" id="homeButton" onClick={() => { handleClick(tag) }}>{tag}</button>)}
+  function Browse() {
+    console.log("Step 1: Load Products in a useState.");
+  
+    const [query, setQuery] = useState('');
+    const [ProductsCategory, setProductsCategory] = useState(Products);
+  
+    const handleChange = (e) => {
+      setQuery(e.target.value);
+      console.log("Step 6 : in handleChange, Target Value :", e.target.value, " Query Value :", query);
+      const results = Products.filter(eachProduct => {
+        if (e.target.value === "") return ProductsCategory;
+        return eachProduct.title.toLowerCase().includes(e.target.value.toLowerCase())
+      });
+      setProductsCategory(results);
+    }
+  
+    function handleClick(tag) {
+      console.log("Step 4 : in handleClick", tag);
+      let filtered = Products.filter(cat => cat.category === tag);
+      // modify useState
+      setProductsCategory(filtered);
+      // ProductsCategory = filtered;
+      console.log("Step 5 : ", Products.length, ProductsCategory.length);
+    }
+  
+    console.log("Step 2: Return App :", Products.length, ProductsCategory.length);
+  
+    return (
+      <div className="flex fixed flex-row">
+        <div className="h-screen bg-slate-800 p-3 xl:basis-1/5" style={{ minWidth: '65%' }}>
+          {/* <img className="w-full" src={logo} alt="Sunset in the mountains" /> */}
+          <div className="px-6 py-4">
+            <h1 className="text-3xl mb-2 font-bold text-white">Assignment 2 Store </h1>
+            <p className="text-gray-700 text-white">
+              by - <b style={{ color: 'mediumseagreen' }}>Jacob Garcia and Kate Endersby</b>
+            </p>
+            <div className="py-10">
+              <input className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg
+                focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700
+                dark:border-gray-600 dark:placeholder-gray-400 dark:text-white
+                dark:focus:ring-blue-500 dark:focus:border-blue-500" type="search" value={query} onChange={handleChange} />
+                <br />
+              {Categories && <p className='text-white'>Tags : </p>}
+              {Categories.map(tag => <button key={tag} className="inline-block bg-amber-600 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mt-2" id="homeButton" onClick={() => { handleClick(tag) }}>{tag}</button>)}
+            </div>
           </div>
         </div>
+        <div className="ml-5 p-10 xl:basis-4/5">
+          {console.log("Before render :", Products.length, ProductsCategory.length)}
+          {render_products(ProductsCategory)}
+        </div>
       </div>
-      <div className="ml-5 p-10 xl:basis-4/5">
-        {console.log("Before render :", Products.length, ProductsCategory.length)}
-        {render_products(ProductsCategory)}
-      </div>
-    </div>
-  );
+    );
+  } // viewer = 0
+
+  function Cart() {
+    return <div></div>;
+  } //viewer = 1
+
+  function Confirmation() {
+    return <div></div>;
+  } //viewer = 2
+
+  return (<div>
+    {viewer === 0 && <Browse />}
+    {viewer === 1 && <Cart />}
+    {viewer === 2 && <Confirmation />}
+  </div>)
 };
 
 export default App;
