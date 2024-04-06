ec01acc800da8e9b69b13b20476a9fefe2febcda
jacobg <jaybob1225@gmail.com>
2024-04-06 15:59:32 -0500

    Minor fix

--- src/App.js
+++ src/App.js
@@ -5,10 +5,6 @@ import { useForm } from "react-hook-form";
 import { Products } from "./Products";
 import { Categories } from "./Categories";
 
-
-
-
-
 const App = () => {
 
   const [viewer, setViewer] = useState(0);
@@ -35,60 +31,8 @@ const App = () => {
     setCartTotal(totalVal);
   };
 
-
-  const render_products = (ProductsCategory) => {
-
-    return <div className='category-section fixed'>
-      {console.log("Step 3 : in render_products ")}
-      <h2 className="text-3xl font-extrabold tracking-tight text-gray-600 category-title">Products ({ProductsCategory.length})</h2>
-  
-      <div className="m-6 p-3 mt-10 ml-0 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-10" style={{ maxHeight: '800px', overflowY: 'scroll', minWidth: '1000px' }}>
-        {/* Loop Products */}
-        {ProductsCategory.map((product, index) => (
-          <div key={index} className="group relative shadow-lg" >
-            <div className=" min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-60 lg:aspect-none">
-              <img
-                alt="Product Image"
-                src={product.image}
-                className="w-full h-full object-center object-cover lg:w-full lg:h-full"
-              />
-            </div>
-            <div className="flex justify-between p-3">
-              <div>
-                <h3 className="text-sm text-gray-700">
-                  <a href={product.href}>
-                    <span aria-hidden="true" className="absolute inset-0" />
-                    <span style={{ fontSize: '16px', fontWeight: '600' }}>{product.title}</span>
-                  </a>
-                  <p>Tag - {product.category}</p>
-                </h3>
-                <p className="mt-1 text-sm text-gray-500">Rating: {product.rating.rate} ({product.rating.count})</p>
-              </div>
-              <p className="text-sm font-medium text-green-600">${product.price}</p>
-            </div>
-            <div id="plusminusbuttons">
-              <center>
-                <button type="button" variant="light" onClick={() => removeFromCart(product)} > - </button>{" "}
-                <button type="button" variant="light" onClick={() => addToCart(product)}> + </button>
-              </center>
-            </div>
-          </div>
-        ))}
-        <p><br/><br/></p>
-      </div>
-    </div>
-  }; // end render_products
-
-  const cartItems = cart.map((el) => (
-    <div key={el.id}>
-      <img class="img-fluid" src={el.image} width={150}/>
-      {el.title} 
-      <div style={{textAlign: "right", fontSize: "20px"}}> <strong>{el.amount} X </strong> ${el.price} = ${el.amount * el.price} </div>
-    </div>
-  ));
-
   const addToCart = (el) => {
-
+    console.log("Adding");
     const cartEl = {
       "id": el.id,
       "title": el.title,
@@ -126,6 +70,57 @@ const App = () => {
     setCart(hardCopy);
     }
   };
+
+  const cartItems = cart.map((el) => (
+    <div key={el.id}>
+      <img class="img-fluid" src={el.image} width={150}/>
+      {el.title} 
+      <div style={{textAlign: "right", fontSize: "20px"}}> <strong>{el.amount} X </strong> ${el.price} = ${el.amount * el.price} </div>
+    </div>
+  ));
+
+  const render_products = (ProductsCategory) => {
+
+    return <div className='category-section fixed'>
+      {console.log("Step 3 : in render_products ")}
+      <h2 className="text-3xl font-extrabold tracking-tight text-gray-600 category-title">Products ({ProductsCategory.length})</h2>
+  
+      <div className="m-6 p-3 mt-10 ml-0 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-10" style={{ maxHeight: '800px', overflowY: 'scroll', minWidth: '1000px' }}>
+        {/* Loop Products */}
+        {ProductsCategory.map((product, index) => (
+          <div key={index} className="group relative shadow-lg" >
+            <div className=" min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-60 lg:aspect-none">
+              <img
+                alt="Product Image"
+                src={product.image}
+                className="w-full h-full object-center object-cover lg:w-full lg:h-full"
+              />
+            </div>
+            <div className="flex justify-between p-3">
+              <div>
+                <h3 className="text-sm text-gray-700">
+                  <a href={product.href}>
+                    <span aria-hidden="true" className="absolute" />
+                    <span style={{ fontSize: '16px', fontWeight: '600' }}>{product.title}</span>
+                  </a>
+                  <p>Tag - {product.category}</p>
+                </h3>
+                <p className="mt-1 text-sm text-gray-500">Rating: {product.rating.rate} ({product.rating.count})</p>
+              </div>
+              <p className="text-sm font-medium text-green-600">${product.price}</p>
+            </div>
+            <div id="plusminusbuttons">
+              <center>
+                <button type="button" variant="light" onClick={() => removeFromCart(product)} > - </button>{" "}
+                <button type="button" variant="light" onClick={() => addToCart(product)}> + </button>
+              </center>
+            </div>
+          </div>
+        ))}
+        <p><br/><br/></p>
+      </div>
+    </div>
+  }; // end render_products
 
   function Browse() {
     console.log("Step 1: Load Products in a useState.");
@@ -208,7 +203,9 @@ const App = () => {
       setPaymentInfo({});
     }
 
-    total();
+    useEffect(() => {
+      total();
+    }, [cart]);
 
     return (
       <div>
